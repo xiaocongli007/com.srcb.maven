f57fb08280146dcc4ba6e0a6c04367bf0ec22d91
xiaocong <xiaocongli1213@163.com>
2022-09-01 15:51:34 +0800

    TestBranch新增内容

---
+++ src/main/java/com/test/TestBranch.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/*
+@Author jiujiu
+@Date 2022/9/1 - 下午3:50
+*/
+public class TestBranch {
+    public static void main(String[] args) {
+        System.out.println("test Branch");
+    }
+}
